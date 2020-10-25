f4765ef6d1f3c0abbf59133b99a62fa060b163c2
Flavia95 <flavia.villani14@gmail.com>
2020-10-25 22:58:36 +0100

    add utils

---
+++ functions/statistics.py
@@ -0,0 +1,50 @@
+import math
+import sys
+sys.path.append("Lab/vgpop/script")
+import vgpop 
+from vgpop import num_sequences
+from vgpop import avg_num_pairwise_differences
+from vgpop import num_segregating_sites
+
+# 5.TajmasD
+
+with open('matrix.tsv', 'r') as f:
+    matrix = [l.strip() for l in f.readlines()]
+
+
+def _tajimas_d(num_sequences, avg_num_pairwise_differences, num_segregating_sites):
+    a1 = sum([1.0/i for i in range(1, num_sequences)])
+    print(a1)
+    a2 = sum([1.0/(i**2) for i in range(1, num_sequences)])
+    b1 = float(num_sequences+1)/(3*(num_sequences-1))
+    b2 = float(2 * ( (num_sequences**2) + num_sequences + 3 )) / (9*num_sequences*(num_sequences-1))
+    c1 = b1 - 1.0/a1
+    c2 = b2 - float(num_sequences+2)/(a1 * num_sequences) + float(a2)/(a1 ** 2)
+    e1 = float(c1) / a1
+    e2 = float(c2) / ((a1**2) + a2)
+
+    D = (
+        float(avg_num_pairwise_differences - (float(num_segregating_sites)/a1))
+        / math.sqrt(
+            (e1 * num_segregating_sites )
+          + ((e2 * num_segregating_sites) * (num_segregating_sites-1) ))
+        )
+    return D
+
+
+
+
+def main():
+    #print(freqgeno(collections)) 
+    #print(freqalle(collections))
+    print(num_sequences(matrix))
+    print(num_segregating_sites(matrix))
+    print(avg_num_pairwise_differences(matrix))
+    print(_tajimas_d(num_sequences(matrix), num_segregating_sites(matrix), avg_num_pairwise_differences(matrix))) 
+   
+    
+    
+if __name__ == "__main__":
+    main()
+
+    
---
+++ functions/vgpop.py
@@ -0,0 +1,86 @@
+from itertools import combinations
+from collections import Counter
+import collections
+
+with open('matrix.tsv', 'r') as f:
+    matrix = [l.strip() for l in f.readlines()]
+
+#1. Return the rows of the matrix 
+def num_sequences(matrix):
+	num_sequences = len(matrix)
+
+	return num_sequences
+
+#2. Return the column of matrix (polymorphic sites of the VG)
+def num_segregating_sites(matrix):
+    num_segregating_sites = len(matrix[0].split(','))
+
+    return num_segregating_sites
+
+#3. Return allele frequency
+def freqalle(collections):
+    allelfreq = []
+    for line in matrix:
+        no_comma = []
+        for c in line:
+            if c == ',':
+                continue
+            no_comma.append(c)
+        
+        occurrences = collections.Counter(no_comma)
+        numerotot = (sum(occurrences.values()))
+
+        for key, value in occurrences.items():
+            allelfreq.append(value/numerotot)
+    return allelfreq
+
+
+#4. Return genotype frequency
+def freqgeno(collections):
+    counter = Counter()
+    freqgeno = []
+
+    for line in matrix:
+        no_comma = []
+        for c in line:
+            if c == ',':
+                continue
+            no_comma.append(c)
+
+            combinseq = (list(combinations(no_comma,2)))
+            occurrencesgeno = collections.Counter(combinseq)
+            numerototgeno = (sum(occurrencesgeno.values()))
+
+        for key, value in occurrencesgeno.items():
+            freqgeno.append(value/numerototgeno)
+    return freqgeno
+
+#5. Returns pair of values: total number of pairwise differences observed between all sequences
+def count_differences(matrix):
+    counter = Counter()
+    combinpath = []
+
+    for p in combinations(matrix, 2):
+        combinpath.append(p)
+
+    count = 0
+
+    for i in range(0, len(combinpath)):
+        for j in range(0, len(combinpath[i][0])):
+            if ',' in combinpath[i][1][j]:
+                continue
+
+            if combinpath[i][0][j] == combinpath[i][1][j]:
+                #print(combinpath[i][0][j], combinpath[i][1][j])
+                print(True)
+            else:
+                count +=1
+                #print(False)    
+
+    return count
+
+
+#6. Return the number of pairwise differences between the sequences and the number of DNA sequences sampled.
+def avg_num_pairwise_differences(matrix):
+    avg_num_pairwise_differences = count_differences(matrix)/num_sequences(matrix)
+    return avg_num_pairwise_differences
